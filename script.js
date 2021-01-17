32b4258ecfe3e19c9bee3705c155cb795ba897ee
netsy001 <nettem.surendra@gmail.com>
2021-01-18 10:15:04 +1030

    changes to ajax method

--- script.js
+++ script.js
@@ -1,19 +1,19 @@
-$(document).ready(function(){
+$(document).ready(function () {
 
-$(".btn").click(function(){
+    $(".btn").click(function() {
+        var inputValue = $("#cityName").val();
+        console.log(inputValue)
+        // var urlName = "http://api.openweathermap.org/data/2.5/weather?q=" + inputValue + "&units=imperial" + "&appid=964304f976bc387e7c28396639c2f8d7";
+        $.ajax({
+            url: "http://api.openweathermap.org/data/2.5/weather?q=" + inputValue + "&units=imperial" + "&appid=964304f976bc387e7c28396639c2f8d7",
+            method: "Get",
+            dataType: "jsonp",
+            success: function(response){
+                console.log(response)
 
-var inputValue = $("#cityName").val("");
-console.log(inputValue)
-var urlName = "https://api.openweathermap.org/data/2.5/weather?q=" + "&inputValue" + "&appid=964304f976bc387e7c28396639c2f8d7"
-console.log(urlName)
-$.ajax({
-url: "urlName",
-method: "Get",
-success: function(result){
-    $("#subject").html(result);
-
-}});
+            }
+        });
+    });
 
 
-});
 });
