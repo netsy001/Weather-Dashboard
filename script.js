b05fd0c36de46c38c1507f8ae389c0d063a8f31b
netsy001 <nettem.surendra@gmail.com>
2021-01-19 10:12:40 +1030

    commiting changes to response parameter

--- script.js
+++ script.js
@@ -1,19 +1,21 @@
-$(document).ready(function () {
+$(document).ready(function (){
 
-    $(".btn").click(function() {
+    $(".btn").click(function(event) {
+        event.preventDefault();
         var inputValue = $("#cityName").val();
-        console.log(inputValue)
-        // var urlName = "http://api.openweathermap.org/data/2.5/weather?q=" + inputValue + "&units=imperial" + "&appid=964304f976bc387e7c28396639c2f8d7";
+    var urlName = "http://api.openweathermap.org/data/2.5/weather?q=" + inputValue + "&units=imperial" + "&appid=964304f976bc387e7c28396639c2f8d7";
         $.ajax({
-            url: "http://api.openweathermap.org/data/2.5/weather?q=" + inputValue + "&units=imperial" + "&appid=964304f976bc387e7c28396639c2f8d7",
+            url: urlName,
             method: "Get",
             dataType: "jsonp",
             success: function(response){
                 console.log(response)
-
+                var currentWeather = show(response);
+                $("$currentConditions").html(currentWeather);
             }
         });
     });
-
-
+            function show(response)
+                return "<li>City Name: + inputValue</li>" + 
+            
 });
