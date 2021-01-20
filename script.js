4eaee31308193bdf7e6edc7e508893e1c4bf8580
netsy001 <nettem.surendra@gmail.com>
2021-01-21 09:03:53 +1030

    changes to script.js

--- script.js
+++ script.js
@@ -1,31 +1,21 @@
-$(document).ready(function (){
-
-    $(".btn").click(function(event) {
+$(document).ready(function () {
+    $(".btn").click(function (event) {
         event.preventDefault();
         var inputValue = $("#cityName").val();
-    var urlName = "http://api.openweathermap.org/data/2.5/weather?q=" + inputValue + "&units=imperial" + "&appid=964304f976bc387e7c28396639c2f8d7";
+        var urlName = "http://api.openweathermap.org/data/2.5/weather?q=" + inputValue + "&units=imperial" + "&appid=964304f976bc387e7c28396639c2f8d7";
         $.ajax({
             url: urlName,
             method: "Get",
             dataType: "jsonp",
-            success: function(response){
-                console.log(response)
-                var currentWeather = show(response);
-                var d = new Date();
-                var localTime = d.getTime();
-                var localOffset = d.getTimezoneOffset();
-                var utc = localTime + localOffset;
-                var date = new Date + utc;
-
-                // show(response) = "<li>City Name: "+ inputValue + "</li>";
+            success: function (response) {
+                var d = moment(response.dt, 'X').utcOffset(response.timezone / 60).format('MMMM Do YYYY');
+                var currentWeather = show(response, inputValue);
                 $("#currentConditions").append(currentWeather);
-                function show(response){
-                    return "<li>City Name: "+ inputValue + "</li>" +
-                $("#currentConditions").val(date);
-                }
-                            
             }
         });
     });
-            
+    function show(response,inputValue) {
+        return "<li>City Name: " + inputValue + "</li>" +
+            "<li>Date: " + response.d + "</li>"; 
+    }
 });
