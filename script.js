79398761e024e5123afdaddb3fac775e60a2ea46
netsy001 <nettem.surendra@gmail.com>
2021-01-21 12:03:15 +1030

    uv index

--- script.js
+++ script.js
@@ -9,7 +9,10 @@ $(document).ready(function () {
             dataType: "jsonp",
             success: function (response) {
                 var d = moment(response.dt, 'X').utcOffset(response.timezone / 60).format('MMMM Do YYYY');
-                var currentWeather = show(response, inputValue,d);
+                var longitude = response.coord.lon;
+                var latitude = response.coord.lat;
+                var uv = "http://api.openweathermap.org/data/2.5/uvi?lat=" + latitude + "&lon=" + longitude + "&appid=964304f976bc387e7c28396639c2f8d7";
+                var currentWeather = show(response, inputValue, d, uv);
                 var iconcode = response.weather[0].icon;
                 var iconurl = "http://openweathermap.org/img/w/" + iconcode + ".png";
                 $("#currentConditions").append(currentWeather);
@@ -17,9 +20,13 @@ $(document).ready(function () {
             }
         });
     });
-    function show(response,inputValue,d) {
-        return "<li>City Name: " + inputValue + "</li>" +
-            "<li>Date: " + d + "</li>"; 
+    function show(response, inputValue, d, uv) {
+        return "<p><b>City Name: " + inputValue + "</b></p>" +
+            "<p>Date: " + d + "</p>" +
+            "<p>Temperature: " + response.main.temp + "</p>" +
+            "<p>Humidity: " + response.main.humidity + "</p>" +
+            "<p>Wind speed: " + response.wind.speed + "</p>" +
+            "<p>UV index: " + uv + "</p>";
     }
-   
+
 });
