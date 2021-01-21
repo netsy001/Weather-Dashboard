40d0cb0960c2e87ceec3939155a06720473101ef
netsy001 <nettem.surendra@gmail.com>
2021-01-21 15:26:12 +1030

    fiveDAYForcast

--- script.js
+++ script.js
@@ -16,25 +16,45 @@ $(document).ready(function () {
                 $("#currentConditions").append(currentWeather);
                 $('#wicon').attr("src", iconurl);
                 var latitude = response.coord.lat;
-            var longitude = response.coord.lon;
-            var uv = "http://api.openweathermap.org/data/2.5/uvi?lat=" + latitude + "&lon=" + longitude + "&appid=964304f976bc387e7c28396639c2f8d7";
-            $.ajax({
-                url: uv,
-                method: "Get",
-                dataType: "jsonp",
-            }).then(function (result) {
-            var uvindex = result.value;
-            $("#currentConditions").text("UV Index:" + uvindex);
+                var longitude = response.coord.lon;
+                var uv = "http://api.openweathermap.org/data/2.5/uvi?lat=" + latitude + "&lon=" + longitude + "&appid=964304f976bc387e7c28396639c2f8d7";
+                $.ajax({
+                    url: uv,
+                    method: "Get",
+                    mode: 'cors',
+                    dataType: "json",
+                }).then(function (result) {
+                    var uvindex = result.value;
+                    $("#currentConditions").append("UV Index: " + uvindex);
                 });
             }
         });
-    });
-    function show(response, inputValue, d) {
-        return "<p><b>City Name: " + inputValue + "</b></p>" +
-            "<p>Date: " + d + "</p>" +
-            "<p>Temperature: " + response.main.temp + "</p>" +
-            "<p>Humidity: " + response.main.humidity + "</p>" +
-            "<p>Wind speed: " + response.wind.speed + "</p>";
-    }
+        function show(response, inputValue, d) {
+            return "<p><b>City Name: " + inputValue + "</b></p>" +
+                "<p>Date: " + d + "</p>" +
+                "<p>Temperature: " + response.main.temp + "</p>" +
+                "<p>Humidity: " + response.main.humidity + "</p>" +
+                "<p>Wind speed: " + response.wind.speed + "</p>";
+        }
+        var fiveUrl = "http://api.openweathermap.org/data/2.5/forecast?q=" + inputValue + "&units=imperial" + "&appid=964304f976bc387e7c28396639c2f8d7";
+        $.ajax({
+            url: fiveUrl,
+            method: "Get",
+            dataType: "jsonp",
+            success: function (data, inputValue, d) {
+                var fiveDayForcast = "";
+                $.each(function (data,inputValue, d) {
+                    fiveDayForcast = 
+                     '<p><b>' + inputValue + '</b></p>' +
+                     '<img src=http://openweathermap.org/img/w/' + data.weather[0].icon + '.png></p>' +
+                    "<p>Date: " + d + "</p>" +
+                    "<p>Temperature: " + data.main.temp + "</p>" +
+                    "<p>Humidity: " + data.main.humidity + "</p>" +
+                    "<p>Wind speed: " + data.wind.speed + "</p>";
+                });
+                $("#futureConditions").append(fiveDayForcast);
 
+            }
+        });
+    });
 });
